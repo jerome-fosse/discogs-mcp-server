2392760092cdb6486eca449e6aa4ea3b139d4221
Jerome Fosse <jerome@fosse.nom.fr>
2026-03-20 12:07:22 +0100

    Configuration tunning

--- src/main/java/com/jf/mcp/discogs/api/DiscogsApi.java
+++ src/main/java/com/jf/mcp/discogs/api/DiscogsApi.java
@@ -1,8 +1,10 @@
 package com.jf.mcp.discogs.api;
 
 import com.jf.mcp.discogs.config.DiscogsApiConfig;
+import com.jf.mcp.discogs.model.MasterRelease;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Component
 public class DiscogsApi {
@@ -12,5 +14,12 @@ public class DiscogsApi {
     public DiscogsApi(DiscogsApiConfig config, WebClient client) {
         this.config = config;
         this.client = client;
+    }
+
+    public Mono<MasterRelease> getMasterReleaseById(String id) {
+        return client.get()
+                .uri("/masters/" + id)
+                .retrieve()
+                .bodyToMono(MasterRelease.class);
     }
 }
--- src/main/java/com/jf/mcp/discogs/config/DiscogsApiConfig.java
+++ src/main/java/com/jf/mcp/discogs/config/DiscogsApiConfig.java
@@ -1,11 +1,15 @@
 package com.jf.mcp.discogs.config;
 
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
 @Configuration
 @ConfigurationProperties(prefix = "discogs.api")
+@Validated
 public class DiscogsApiConfig {
+    @NotBlank
     private String token;
     private String baseUrl;
     private Integer timeout;
@@ -51,6 +55,6 @@ public class DiscogsApiConfig {
                    timeout: %s
                    pageSize: %s
                 """
-                .formatted("**********", baseUrl, timeout, pageSize);
+                .formatted("***************", baseUrl, timeout, pageSize);
     }
 }
